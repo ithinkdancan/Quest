2383b27df6f36e9fc0d76e714caee2cb92d85e42
Daniel Tijerina <ithinkdancan@gmail.com>
2013-08-17 09:19:40 -0500

    angular dependencies

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,12 @@
 
 angular.module('questApp')
   .controller('MainCtrl', function ($scope) {
+   
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      'Esmeralda',
+      'Daniel',
+      'Kingsley',
+      'Roxy'
     ];
+
   });
