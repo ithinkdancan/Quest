623628d622de93d82fd0a976e03a047e1944336e
Daniel Tijerina <daniel.tijerina@atomicaxis.com>
2013-08-18 09:49:15 -0500

    sample grail list

---
+++ grails.js
@@ -0,0 +1,41 @@
+[
+	{
+		name: 'Maddam Mams'
+	},{
+		name: 'Korea House'
+	},{
+		name: 'Mister Tramps'
+	},{
+		name: 'Top Notch'
+	},{
+		name: 'Torchys'
+	},{
+		name: 'Zen'
+	},{
+		name: 'Pinthouse Pizza'
+	},{
+		name: 'Phonatic'
+	},{
+		name: 'Dos Batos'
+	},{
+		name: 'Elevation Burger'
+	},{
+		name: 'PhoVan'
+	},{
+		name: 'Schlotzkys'
+	},{
+		name: 'Jimmy Johns'
+	},{
+		name: 'Mighty Fine'
+	},{
+		name: 'P. Terrys'
+	},{
+		name: 'Whataburger'
+	},{
+		name: 'Hopdoddy'
+	},{
+		name: 'Verts'
+	},{
+		name: 'Trudy's
+	}
+]
