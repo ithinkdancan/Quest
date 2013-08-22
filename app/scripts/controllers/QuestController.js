09f3ee1aa17b674eacba92290dcd81abe39f4e72
Daniel Tijerina <ithinkdancan@gmail.com>
2013-08-22 08:17:24 -0500

    JSHint

--- app/scripts/controllers/QuestController.js
+++ app/scripts/controllers/QuestController.js
@@ -9,32 +9,29 @@ angular.module('questApp')
     $scope.winner = QuestService.get('quest:complete');
 
     $scope.selectedGrails = [];
-    // $scope.grails = QuestService.get('grails:list').then(function(obj){
-    // 	return getRandomSubarray(obj,9);
-    // })
 
     $scope.$on('socket:quest:leave', function(){
-        window.location = '/';
+      window.location = '/';
     });
 
     $scope.$on('socket:quest:update', function(event, obj){
-  		$scope.quest = obj;
+      $scope.quest = obj;
       if($scope.quest.started && !$scope.selectedGrails.length){
         QuestService.get('grails:list')
-          .then(function(obj){ 
-            $scope.grails = obj; 
-          });;
+          .then(function(obj){
+            $scope.grails = obj;
+          });
         $state.transitionTo('quest.start', $stateParams);
       }
-  	});
+    });
 
     var saveGrails = function () {
       socket.emit('quest:save', $scope.selectedGrails);
-    }
+    };
 
-  	$scope.startQuest = function () {
-  		socket.emit('quest:start');
-  	}
+    $scope.startQuest = function () {
+      socket.emit('quest:start');
+    };
 
     $scope.pickGrail = function (_id) {
       
@@ -48,6 +45,6 @@ angular.module('questApp')
       if(!$scope.grails.length){
         saveGrails();
       }
-    }
+    };
 
   });
