54c14bc2fdf672b6a98f6110ff5c5ff861943af4
Daniel Tijerina <daniel.tijerina@atomicaxis.com>
2013-10-07 14:34:17 -0500

    updated stats runner

--- stats.js
+++ stats.js
@@ -7,29 +7,68 @@
 	var grailsObject = {};
 
 	var champions = {};
+	var votes = {};
 	var arrayResults = [];
+	var arrayVotesResults = [];
 
 
 	//Get Top 5 Winners
 	var topFive = function(){
 		quests.find({'champion': {$ne : false} }).toArray(function(error, results){
 
-			//tabulate the results
+			//tabulate the winner results
 			results.forEach(function(e, i, a){
+				
 				if(e.champion){
 					champions[e.champion] = champions[e.champion] ? champions[e.champion]+1 : 1;
 				}
+
+				if(e.votes){
+					e.votes.forEach(function(e1){
+						e1.forEach(function(e2){
+							votes[e2] = votes[e2] ? votes[e2]+1 : 1;
+						})
+					})
+				}
 			});
 
-			//sort the results
+
+			//sort the champion results
 			for (o in champions){
 				arrayResults.push({id:o, name: grailsObject[o], votes: champions[o]});
 			}
-			arrayResults = arrayResults.sort(function(a,b){ return b.votes - a.votes }).splice(0,5);
+			arrayResults = arrayResults.sort(function(a,b){ return b.votes - a.votes });
 
-			console.log(arrayResults)
+			//sort the votes results
+			for (o in votes){
+				arrayVotesResults.push({id:o, name: grailsObject[o], votes: votes[o]});
+			}
+			arrayVotesResults = arrayVotesResults.sort(function(a,b){ return b.votes - a.votes });
+
+			console.log("\n\nGrails By Wins\n-----------");
+			arrayResults.forEach(function(e){
+				console.log(e.name + ': ' + e.votes);
+			})
+
+			console.log("\n\nGrails By Votes\n-----------");
+			arrayVotesResults.forEach(function(e){
+				console.log(e.name + ': ' + e.votes);
+			})
+
+			console.log("\n\n");
+
+			// console.log(arrayResults);
+			// console.log(arrayVotesResults)
+
+			process.exit();
 
 		});
+	}
+
+	var totalVotes = function () {
+		quests.find({'champion': {$ne : false} }).toArray(function(error, results){
+
+		})
 	}
 		
 
